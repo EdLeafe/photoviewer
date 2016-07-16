623ce02f40adbab8d3bde5321d87dd4049b6f33d
EdLeafe <ed@leafe.com>
2016-07-16 16:53:16 -0500

    Forgot to add this class file

---
+++ fullimage.py
@@ -0,0 +1,47 @@
+#/usr/bin/env python
+
+import dabo
+
+
+class FullImage(dabo.ui.dImage):
+    def fill(self, evt=None):
+        """ Size and place this image to fill its parent."""
+        parent = self.Parent
+        pW, pH = parent.Size
+        pW, pH = float(pW), float(pH)
+        iW, iH = origW, origH = self.Size
+        iW, iH = float(iW), float(iH)
+        pProp = pW / pH
+        iProp = iW / iH
+
+        szW = pW
+        szH = pW / iProp
+        pad = diff = None
+        if szH > pH:
+            # Image is taller than parent; use the height as the limit
+            szH = pH
+            szW = pH * iProp
+            pad = "W"
+            diff = pW - szW
+        elif szH < pH:
+            # Width is the limiting dimension
+            pad = "H"
+            diff = pH - szH
+
+        if (origW, origH) == (szW, szH):
+            # Size hasn't changed
+            return
+
+        self.Form.lockDisplay()
+        # Resize the image
+        self.Size = szW, szH
+        # Position on parent
+        if pad == "W":
+            self.Position = ((diff / 2), 0)
+        elif pad == "H":
+            self.Position = (0, (diff / 2))
+        else:
+            # Proportions match
+            self.Position = (0, 0)
+        dabo.ui.callAfterInterval(150, self.Form.unlockDisplay)
+
