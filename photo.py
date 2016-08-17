efdbc0f39bdd7f4d78836b5badf45468fa12df03
EdLeafe <ed@leafe.com>
2016-08-17 13:53:24 -0500

    began adding config support

--- photo.py
+++ photo.py
@@ -1,4 +1,7 @@
 #/usr/bin/env python
+from __future__ import print_function
+
+import six.moves.configparser as ConfigParser
 
 import dabo
 dabo.ui.loadUI("wx")
@@ -13,17 +16,39 @@ class ImgForm(dabo.ui.dForm):
     def beforeInit(self):
         self.SaveRestorePosition = False
         self.ShowStatusBar = False
+        self._read_config
 
     def afterInit(self):
 #        self.WindowState = "Fullscreen"
         self.WindowState = "normal"
         self.Size = (400, 300)
-        self.BackColor = "red"
+        self.BackColor = "black"
         self.mainPanel = mp = dabo.ui.dPanel(self, BackColor="black")
         self.Sizer.append1x(mp)
         mp.Sizer = sz = dabo.ui.dSizer("v")
         self.img = FullImage(mp, Picture=PHOTO)
         mp.bindEvent(dabo.dEvents.Resize, self.img.fill)
+
+    def _read_config(self):
+        cfg = ConfigParser.SafeConfigParser()
+        try:
+            cfg.read(config_file)
+        except ConfigParser.MissingSectionHeaderError as e:
+            # The file exists, but doesn't have the correct format.
+            raise exc.InvalidConfigurationFile(e)
+
+        def safe_get(section, option, default=None):
+            try:
+                return cfg.get(section, option)
+            except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+                return default
+
+        self.host_url = safe_get("host", "url")
+        if not self.host_url:
+            print("No host URL configured in photo.cfg; exiting")
+            exit()
+        else:
+            print("Yay! %s" % self.host_url)
 
 
 if __name__ == "__main__":
