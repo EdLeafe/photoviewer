d529e2032b6be50141f95747ccb002177726d0b9
EdLeafe <ed@leafe.com>
2019-07-24 12:46:23 -0500

    Updates to the install process

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+echo Creating the virtualenv...
 python3 -m venv vphoto
+source vphoto/bin/activate
+echo Installing requirements...
+pip install -U pip
 pip install -r requirements.txt
+
+cp photo.cfg.template photo.cfg
+UUID=$(cat /proc/sys/kernel/random/uuid)
+echo "UUID: $UUID"
+sed -i -e "s/PKID/$UUID/g" photo.cfg
+echo Remember to set the frame name and description before starting the service.
+#echo "Name for this photo frame: "
+#read frame_name
+#read -p "Description: " desc
+#sed -i -e "s/FRAMENAME/$frame_name/g" photo.cfg
+#sed -i -e "s/FRAMEDESC/$desc/g" photo.cfg
