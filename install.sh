f532822dff10fd8ab3fce0658d282a2ccae3a560
EdLeafe <ed@leafe.com>
2020-05-31 18:00:41 -0500

    Added the autostart changes so that Chromium is launched in kiosk mode

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ echo Installing requirements...
 pip install -U pip setuptools wheel
 pip install -r requirements.txt
 
+# Copy the autostart file
+sudo cp autostart.txt /etc/xdg/lxsession/LXDE-pi/autostart
+
 # Copy the service file
 sudo cp photoviewer.service /lib/systemd/system/
 sudo systemctl daemon-reload
@@ -17,6 +20,7 @@ UUID=$(cat /proc/sys/kernel/random/uuid)
 echo "UUID: $UUID"
 sed -i -e "s/PKID/$UUID/g" photo.cfg
 echo Remember to set the frame name and description before starting the service.
+echo You will need to restart the system for the changes to take effect
 #echo "Name for this photo frame: "
 #read frame_name
 #read -p "Description: " desc
